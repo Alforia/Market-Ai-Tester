5e30c13a119430381f8fcac221ec8daf97fbdc1e
akashkottil <kottilakash@gmail.com>
2024-07-29 17:22:41 +0530

    deployment testing

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Market-Ai-Tester/', // Replace with your repository name
+  base: '/', // Replace with your repository name
 });
