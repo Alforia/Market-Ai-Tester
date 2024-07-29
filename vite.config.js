e4202e8b7e07805dfd27e1ac74f2dfd41bc70891
akashkottil <kottilakash@gmail.com>
2024-07-29 17:20:15 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Replace with your repository name
+  base: '/Market-Ai-Tester/', // Replace with your repository name
 });
