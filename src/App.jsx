2a97abfaf62f28dc84284cef9167486678a903d2
akashkottil <kottilakash@gmail.com>
2024-07-29 17:28:20 +0530

    routing bug fixed by adding hashrouting

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, } from 'react-router-dom';
 import LandingPage from './Pages/LandingPage'
 import Explore from './Pages/Explore'
 import LoginPage from './Pages/LoginPage';
